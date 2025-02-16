2dbd61dd2f36bc5cabd999d6283e69a13f209b09
sol572 <solomon.bailey@npbhs.school.nz>
2025-02-17 12:04:35 +1300

    Create C_01_Temp_Gui_v2.py

---
+++ C_01_Temp_Gui_v2.py
@@ -0,0 +1,75 @@
+from tkinter import *
+
+class Converter():
+    """
+    Temperature conversion tool (C to F or F to C)
+    """
+
+    def __init__(self):
+        """
+        Temperature converter GUI
+        """
+
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame,
+                                  text="Temperature Converter",
+                                  font=("Arial", "16", "bold")
+                                  )
+        self.temp_heading.grid(row=0)
+        
+        instructions = ("Please enter a temperature below and then press" 
+                        " one of the buttons to convert it from centigrade" 
+                        " to Fahrenheit")
+        
+        self.temp_instructions = Label(self.temp_frame,
+                                       text=instructions,
+                                       wraplength=250, width=40,
+                                       justify="left")
+        self.temp_instructions.grid(row=1)
+
+        self.temp_entry = Entry(self.temp_frame,
+                                font=("Arial", "14")
+                                )
+        self.temp_entry.grid(row=2, padx=10, pady=10)
+
+        error = "Please enter a number"
+        self.temp_error = Label(self.temp_frame, text=error,
+                                fg="#9C0000")
+        self.temp_error.grid(row=3)
+
+        # Concersion, help and history / export buttons
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        # button list (button text | bg colour | command | row | column)
+        button_details_list = [
+            ["To Celsius", "#990099", "", 0, 0],
+            ["To Fahrenheit", "#008A00", "", 0, 1],
+            ["Help / Info", "#FFAA00", "", 1, 0],
+            ["History / Export", "#004C99", "", 1, 1]
+        ]
+
+        # List to hold buttons once they have been made
+        self.button_ref_list = []
+
+        for item in button_details_list:
+            self.make_button = Button(self.button_frame,
+                                      text=item[0], bg=item[1],
+                                      fg="#FFFFFF", font=("Arial", "12", "bold"),
+                                      width=12, command=item[2])
+            self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
+
+            self.button_ref_list.append(self.make_button)
+
+        # retrieve 'history / export' button and disable it at the start
+        self.to_history_button = self.button_ref_list[3].config(state=DISABLED)
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
