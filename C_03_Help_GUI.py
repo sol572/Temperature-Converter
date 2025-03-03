b0c8293e3d8522250358906a2fbe802b2ff33ece
sol572 <solomon.bailey@npbhs.school.nz>
2025-03-04 10:53:58 +1300

    Help GUI

--- C_03_Help_GUI.py
+++ C_03_Help_GUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from functools import partial # To prevent unwanted windows
 
 
 class Converter:
@@ -23,16 +24,30 @@ class Converter:
         self.to_help_button.grid(row=1, padx=5, pady=5)
 
     def to_help(self):
-        DisplayHelp()
+        """
+        Opens help dialogue box and disables help button
+        (so that users can't create multiple help boxes).
+        """
+        DisplayHelp(self)
 
 
 class DisplayHelp:
+    """
+    Displays help dialogue box
+    """
 
-    def __init__(self):
-
+    def __init__(self, partner):
         # setup dialouge box and background colour
         background = "#ffe6cc"
         self.help_box = Toplevel()
+
+        # disable help button
+        partner.to_help_button.config(state=DISABLED)
+
+        # If users press cross at top, closes help and
+        # 'releases' help button
+        self.help_box.protocol('WM_DELETE_WINDOW',
+                              partial(self.close_help, partner)) 
 
         self.help_frame = Frame(self.help_box, width=300,
                                 height=200)
@@ -44,16 +59,28 @@ class DisplayHelp:
         self.help_heading_label.grid(row=0)
 
 
-        help_text =
+        help_text = "To use the program, simply enter the temperature " \
+                    "you wish to convert and then choose to convert " \
+                    "to either degrees Celsius (centigrade) or " \
+                    "Fahrenheit.. \n\n" \
+                    " Note that -273 degrees C " \
+                    "(-459 F) is absolute zero (the coldest possible " \
+                    "temperature). If you try to convert a " \
+                    "temperature that is less than -273 degrees C, " \
+                    "you will get an error message. \n\n " \
+                    "To see your " \
+                    "calculation history and export it to a text " \
+                    "file, please click the 'History / Export' button."
         self.help_text_label = Label(self.help_frame,
-                                     text="help text goes here", wraplength=350,
+                                     text=help_text, wraplength=350,
                                      justify="left")
         self.help_text_label.grid(row=1, padx=10)
 
         self.dismiss_button = Button(self.help_frame,
                                      font=("Arial", "12", "bold"),
                                      text="Dismiss", bg="#CC6600",
-                                     fg="#FFFFFF", command=self.close_help)
+                                     fg="#FFFFFF", 
+                                     command=partial(self.close_help, partner))
         self.dismiss_button.grid(row=2, padx=10, pady=10)
 
         # List and loop to set background colour on
@@ -64,7 +91,12 @@ class DisplayHelp:
         for item in recolour_list:
             item.config(bg=background)
 
-    def close_help(self):
+    def close_help(self, partner):
+        """
+        Closes help dialogue box (and enables help button)
+        """
+        # Put help button back to normal...
+        partner.to_help_button.config(state=NORMAL)
         self.help_box.destroy()
 
 # main routine
